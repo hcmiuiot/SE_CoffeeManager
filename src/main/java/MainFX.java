f9f9fe959e0d953451c5d68b057c301b2e9eb3c2
Khoa Tran <tranduckhoatcu@gmail.com>
2018-11-10 16:50:12 +0700

    Update UI

--- src/main/java/MainFX.java
+++ src/main/java/MainFX.java
@@ -16,7 +16,7 @@ public class MainFX extends Application {
     @Override
     public void start(Stage primaryStage) {
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("MainForm.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Ui.fxml"));
             Parent root = loader.load();
             Scene mainScene = new Scene(root,400,400);
             primaryStage.setScene(mainScene);
