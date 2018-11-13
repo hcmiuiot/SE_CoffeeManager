adbf0aaf1204e81fb0e517bc5708317ccb5442ed
Nguyễn Phan Hùng Thuận <hungthuanmk@gmail.com>
2018-11-13 22:20:05 +0700

    Add more test

--- src/main/java/Launch.java
+++ src/main/java/Launch.java
@@ -24,6 +24,5 @@ public class Launch extends Application {
 
     public static void main(String[] args) {
         launch(args);
-
     }
 }
--- src/test/java/LaunchTest.java
+++ src/test/java/LaunchTest.java
@@ -1,21 +1,37 @@
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
+import org.junit.After;
 import org.junit.Test;
+import org.testfx.api.FxToolkit;
 import org.testfx.framework.junit.ApplicationTest;
+
+import java.util.concurrent.TimeoutException;
 
 import static org.testfx.api.FxAssert.verifyThat;
 import static org.testfx.matcher.control.LabeledMatchers.hasText;
 
 public class LaunchTest extends ApplicationTest {
 
+//    @Before
+//    public void setUpClass() throws Exception {
+//        ApplicationTest.launch(Launch.class);
+//    }
+    @After
+    public void afterEachTest() throws TimeoutException {
+        FxToolkit.hideStage();
+        release(new KeyCode[]{});
+        release(new MouseButton[]{});
+    }
+
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("MainForm.fxml"));
         Scene scene = new Scene(root);
-        stage.initStyle(StageStyle.UNDECORATED);
+        //stage.initStyle(StageStyle.UNDECORATED);
         stage.setScene(scene);
         stage.show();
         scene.getStylesheets().getClass().getResource("Chart.css");
@@ -24,6 +40,12 @@ public class LaunchTest extends ApplicationTest {
     @Test
     public void should_test() {
         verifyThat(".button", hasText("DashBoard"));
+    }
+
+    @Test
+    public void should_test_2() {
+        clickOn("Drinks");
+        //verifyThat(".button", hasText("Exit"));
     }
 
 }
