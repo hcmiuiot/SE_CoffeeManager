d1ccadcfb49bf87c7d9d4ca5f748f95fd895bcab
Khoa Tran <tranduckhoatcu@gmail.com>
2018-12-23 23:06:33 +0700

    new branch

--- src/main/java/DrinksController.java
+++ src/main/java/DrinksController.java
@@ -1,26 +1,43 @@
-import com.jfoenix.controls.JFXTreeTableView;
+import com.jfoenix.controls.*;
 import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.StackPane;
+import javafx.util.Callback;
 
 import java.io.IOException;
 import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.ResourceBundle;
+import java.util.function.Predicate;
 
 public class DrinksController implements Initializable {
     public static ResultSet result;
-
+    public Parent fxml = null;
+    @FXML
+    private JFXTreeTableView<Drink> treeView;
+    @FXML
+    private JFXButton sort;
+    @FXML
+    private JFXButton shopping_cart;
     @FXML
     private AnchorPane main;
     @FXML
@@ -29,11 +46,20 @@ public class DrinksController implements Initializable {
     private ScrollPane scroll_pane;
     @FXML
     private FlowPane container;
+    @FXML
+    private StackPane shoppingcart_pane;
+//    @FXML
+//    private JFXButton hide_shoppingcartpane_button;
+
+    public static ObservableList<Drink> drinks = FXCollections.observableArrayList();
+
+
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-
-        Parent fxml = null;
+        shoppingcart_pane.setVisible(false);
         loadDB();
+
         try {
             while (result.next()) {
                 fxml = FXMLLoader.load(getClass().getResource("item.fxml"));
@@ -48,15 +74,17 @@ public class DrinksController implements Initializable {
 
         } catch (IOException e) {
             e.printStackTrace();
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
 
+
     }
+
 
     @FXML
     private void filter(ActionEvent event) {
+
     }
 
     public void loadDB() {
@@ -66,12 +94,94 @@ public class DrinksController implements Initializable {
             result.first();
 
 
-
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
 
+    @FXML
+    void open_shopping_cart(MouseEvent event) {
+        JFXTreeTableColumn<Drink, String> NameCol = new JFXTreeTableColumn<>("Name");
+        NameCol.setPrefWidth(150);
+        NameCol.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<Drink, String>, ObservableValue<String>>() {
+            @Override
+            public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<Drink, String> param) {
+                return param.getValue().getValue().Tile;
+            }
+        });
+
+        JFXTreeTableColumn<Drink, String> priceCol = new JFXTreeTableColumn<>("Price");
+        priceCol.setPrefWidth(150);
+        priceCol.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<Drink, String>, ObservableValue<String>>() {
+            @Override
+            public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<Drink, String> param) {
+                return param.getValue().getValue().price;
+            }
+        });
+
+        //drinks.add(new Drink("khoa","khoa","khoa","khoa"));
+        final TreeItem<Drink> root = new RecursiveTreeItem<Drink>(drinks, RecursiveTreeObject::getChildren);
+        treeView.getColumns().setAll( NameCol,priceCol);
+        treeView.setRoot(root);
+        treeView.setShowRoot(false);
+        treeView.setVisible(false);
+        shoppingcart_pane.setVisible(false);
+        JFXDialogLayout content = new JFXDialogLayout();
+        content.setHeading(new Label("Shopping Cart"));
+        content.setBody(treeView);
+        JFXDialog dialog = new JFXDialog(shoppingcart_pane,content, JFXDialog.DialogTransition.TOP);
+        JFXButton button = new JFXButton("OK");
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                dialog.close();
+                treeView.setVisible(false);
+                shoppingcart_pane.setVisible(false);
+            }
+        });
+        content.setActions(button);
+        dialog.show();
+        shoppingcart_pane.setVisible(true);
+        treeView.setVisible(true);
+
+    }
+//    @FXML
+//    void hide_shoppingcart_pane(MouseEvent event) {
+//    shoppingcart_pane.setVisible(false);
+//    }
+
+
+
 }
+class Drink extends RecursiveTreeObject<Drink> {
+
+    StringProperty Tile;
+    StringProperty description;
+    StringProperty price;
+    StringProperty image;
+
+
+    public Drink(String Tile, String description, String price,String image) {
+        this.Tile = new SimpleStringProperty(Tile);
+        this.description = new SimpleStringProperty(description);
+        this.price = new SimpleStringProperty(price);
+        this.image = new SimpleStringProperty(image);
+
+
+
+    }
+    public String getTileName(){
+        return Tile.get();
+    }
+    public String getDescription(){
+        return description.get();
+    }
+    public String getPrice(){
+        return price.get();
+    }
+    public String getImage(){
+        return image.get();
+    }
+}
+
--- src/main/java/ItemController.java
+++ src/main/java/ItemController.java
@@ -1,6 +1,4 @@
-import com.jfoenix.controls.JFXTreeTableColumn;
-import com.jfoenix.controls.JFXTreeTableView;
-import com.jfoenix.controls.RecursiveTreeItem;
+import com.jfoenix.controls.*;
 import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
@@ -9,13 +7,16 @@ import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeTableColumn;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 import javafx.util.Callback;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.Parent;
@@ -32,9 +33,12 @@ import java.util.function.Predicate;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.StackPane;
 
+
 public class ItemController implements Initializable {
+    DrinksController drinkcontroller = new DrinksController();
 
-
+    @FXML
+    private AnchorPane itemPane;
 
     @FXML
     private Label Tile;
@@ -51,9 +55,17 @@ public class ItemController implements Initializable {
     @FXML
     private ImageView imgView;
 
+    @FXML
+    private JFXButton add_button;
+
+    @FXML
+    private StackPane dialog_pane;
+    public String Tilename;
+    public String Price;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        dialog_pane.setVisible(false);
         try {
             Tile.setText(DrinksController.result.getString("name"));
             description.setText(DrinksController.result.getString("description"));
@@ -62,15 +74,49 @@ public class ItemController implements Initializable {
             imgView.setFitHeight(100);
             imgView.setFitWidth(100);
             image_box.getChildren().add(imgView);
+            Tilename = Tile.getText();
+            Price = price.getText();
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        //System.out.println(itemPane.getId());
     }
 
 
 
     @FXML
     private void filter(ActionEvent event) {
+    }
+
+    @FXML
+    void add_item(MouseEvent event) {
+        drinkcontroller.drinks.add(new Drink(Tilename,Price,Price,Price));
+        JFXDialogLayout content = new JFXDialogLayout();
+        content.setHeading(new Label(Tile.getText()));
+        Label descrip = new Label();
+        descrip.setText("Ok my babe");
+        descrip.setWrapText(true);
+        content.setBody(descrip);
+        JFXDialog dialog = new JFXDialog(dialog_pane,content, JFXDialog.DialogTransition.CENTER);
+        JFXButton button = new JFXButton("OK");
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                dialog.close();
+            }
+        });
+
+//            AnchorPane newLoadedPane = FXMLLoader.load(getClass().getResource("item.fxml"));
+            Label label = new Label("text");
+            label.setUserData("text");
+            label.setId("text");
+
+//            DrinksController.container.getChildren().add(label);
+
+        dialog_pane.setVisible(true);
+        content.setActions(button);
+        dialog.show();
     }
 
 }
